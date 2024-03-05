383cbf81ee2ab2cc472dce1ed3d1927a2d1391a9
Syphron <julian.odermatt@gmx.ch>
2024-03-05 09:36:58 +0100

    added todo page

--- src/main.js
+++ src/main.js
@@ -9,14 +9,16 @@ import WeatherApp from "@/pages/WeatherApp.vue";
 import Home from "@/pages/Home.vue";
 import StartupNameGenerator from "@/pages/StartupNameGenerator.vue";
 import InvestmentRechner from "@/pages/InvestmentRechner.vue";
+import Todo from './pages/Todo.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component:  Home},
-        { path: '/weather', component: WeatherApp},
-        { path: '/namegenerator', component: StartupNameGenerator},
-        { path: '/investmentrechner', component: InvestmentRechner},
+        { path: '/', component: Home },
+        { path: '/weather', component: WeatherApp },
+        { path: '/namegenerator', component: StartupNameGenerator },
+        { path: '/investmentrechner', component: InvestmentRechner },
+        { path: '/todo', component: Todo },
     ]
 });
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,19 +5,18 @@
 <template>
 
   <div class="title">
-  <h1>Ük Aufgaben M294</h1>
+    <h1>Ük Aufgaben M294</h1>
   </div>
   <div class="elements">
     <router-link to="/weather">Weather App (Aufgabe 1)</router-link>
     <router-link to="/namegenerator">Name Generator (Aufgabe 2)</router-link>
     <router-link to="/investmentrechner">Investment Rechner (Aufgabe 3)</router-link>
-
+    <router-link to="/todo">ToDo (Aufgabe 4)</router-link>
   </div>
 
 </template>
 
 <style scoped>
-
 * {
   font-family: "Arial";
 }
@@ -31,7 +30,7 @@
   justify-content: center;
   align-items: center;
   grid-template-columns: repeat(3, 1fr);
+  row-gap: 5rem;
   margin-top: 1rem;
 }
-
 </style>
